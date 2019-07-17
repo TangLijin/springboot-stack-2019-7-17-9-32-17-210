dca8eb2560f2d02b7945962d66410e21d31a0000
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-17 20:22:20 +0800

    test:should_return_all_cases_order_by_happen_time

--- src/test/java/com/tw/apistackbase/repository/CaseRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/repository/CaseRepositoryTest.java
@@ -3,12 +3,14 @@ package com.tw.apistackbase.repository;
 import com.tw.apistackbase.entity.Case;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -19,9 +21,9 @@ public class CaseRepositoryTest {
     @Autowired
     private CaseRepository caseRepository;
 
-//    @BeforeEach
-//    public void init(){
-//        caseRepository.deleteAll();
+   @BeforeEach
+    public void init(){
+        caseRepository.deleteAll();
 //        Case case1 = new Case("case1",123456789L);
 //        Case case2 = new Case("case2",223456789L);
 //        Case case3 = new Case("case3",323456789L);
@@ -32,7 +34,7 @@ public class CaseRepositoryTest {
 //        caseRepository.save(case3);
 //        caseRepository.save(case4);
 //        caseRepository.save(case5);
-//    }
+    }
 
 
     @Test
@@ -64,5 +66,26 @@ public class CaseRepositoryTest {
         Assertions.assertEquals(expect.getId(),case2.getId());
         Assertions.assertEquals(expect.getCaseName(),case2.getCaseName());
         Assertions.assertEquals(expect.getHappenTime(),case2.getHappenTime());
+    }
+
+    @Test
+    public void should_return_all_cases_order_by_happen_time(){
+        //given
+        Case case1 = new Case("case1",123456789L);
+        Case case2 = new Case("case2",223456789L);
+        Case case3 = new Case("case3",323456789L);
+        caseRepository.save(case1);
+        caseRepository.save(case2);
+        caseRepository.save(case3);
+        
+        //when
+        List<Case> orderList = (List<Case>)caseRepository.findAll();
+        orderList.sort(Comparator.comparing(Case::getHappenTime).reversed());
+
+        //then
+        Assertions.assertSame(case1,orderList.get(2));
+        Assertions.assertSame(case2,orderList.get(1));
+        Assertions.assertSame(case3,orderList.get(0));
+
     }
 }
